7ab98fe22101e43b997556cdd84c7799f725f6a7
sertanrdn <sertanerdogan07@gmail.com>
2025-05-01 12:53:11 +0200

    Adding routes for pages and global styling

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export function NavBar() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/wishlist">Wishlist</Link>
+        </nav>
+    );
+}
