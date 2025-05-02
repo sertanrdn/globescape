7b77253ff27cd333cc3fac6f7d365533cd359bba
sertanrdn <sertanerdogan07@gmail.com>
2025-05-02 16:31:45 +0200

    Styling the country modal

--- src/components/CountryModal.jsx
+++ src/components/CountryModal.jsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
 import { IoClose } from 'react-icons/io5';
+import '../styles/CountryModal.css';
 
 Modal.setAppElement('#root');
 
@@ -22,16 +23,14 @@ export function CountryModal({ isOpen, onRequestClose, countryName, countryData,
                 <div>
                     <h2>{countryName}</h2>
                     <img src={country.flags.png} alt={`Flag of ${countryName}`} />
-                    <p><strong>Official Name:</strong>{country.name.official}</p>
-                    <p><strong>Capital:</strong>{country.capital?.[0]}</p>
-                    <p><strong>Continent:</strong>{country.continents?.[0]}</p>
-                    <p><strong>Population:</strong>{country.population.toLocaleString()}</p>
-                    <p><strong>Languages:</strong>{country.languages && Object.values(country.languages).join(', ')}</p>
-                    <p><strong>Currency:</strong>{country.currencies && Object.values(country.currencies).map(cur => cur.name).join(', ')}</p>
+                    <p><strong>Official Name: </strong>{country.name.official}</p>
+                    <p><strong>Capital: </strong>{country.capital?.[0]}</p>
+                    <p><strong>Continent: </strong>{country.continents?.[0]}</p>
+                    <p><strong>Population: </strong>{country.population.toLocaleString()}</p>
+                    <p><strong>Languages: </strong>{country.languages && Object.values(country.languages).join(', ')}</p>
+                    <p><strong>Currency: </strong>{country.currencies && Object.values(country.currencies).map(cur => cur.name).join(', ')}</p>
                 </div>
             )}
-
-            <p>Details about the country</p>
         </Modal>
     );
 }
